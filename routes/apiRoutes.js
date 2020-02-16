052c4688cd90fc0858dcda39e1a2aeabb5db4f75
Tucker Vassau <tuckv@hotmail.com>
2020-02-15 21:29:43 -0700

    FIXED, all functions working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,39 @@
 const path = require("path");
 const fs = require("fs");
 
+const uuid = require("uuid");
+
+
 module.exports = function(app){
 
-    app.get("/api/notes", function(err, res){
-        if (err) throw err;
-        res.sendFile(path.join(__dirname, "./db/db.json"));
+    app.get("/api/notes", function(req, res){
+        // if (err) throw err;
+        res.sendFile(path.join(__dirname, "../db/db.json"));
     });
 
 
     // 
     app.post("/api/notes", function(req, res){
-        let newNote = req.body;
+
+        let noteId = uuid();
+
+        let newNote = {
+          id: noteId,
+          title: req.body.title,
+          text: req.body.text
+        };
 
         fs.readFile("./db/db.json", "utf-8", (err, data) =>{
-            if (err) throw err;
-            let db = JSON.parse(data);
-            db.push(newNote);
-            let idKey = 1;
-            for(var i = 0; i < db.length; i++){
-                db[i].id = idKey++;
-            }
 
-            fs.writeFile("./db/db.json", JSON.stringify(db), function(err){
+            if (err) throw err;
+          
+            const notesDb = JSON.parse(data);
+
+            notesDb.push(newNote);
+
+            fs.writeFile("./db/db.json", JSON.stringify(notesDb, null, 2), function(err){
                 if (err) throw err;
-                return res.status(200).send("Added new Note");
+                res.send(notesDb);
             })
          })
        
@@ -32,10 +41,11 @@ module.exports = function(app){
     // 
 
 
-    app.delete("/api/notes:id", function(req, res){
+    app.delete("/api/notes/:id", function(req, res){
         let specificNoteId = req.params.id;
-        fs.readFile("./db/db.json", "uft-8", (err, data), err =>{
+        fs.readFile("./db/db.json", "utf8", (err, data) =>{
             if (err) throw err;
+            
             let db = JSON.parse(data);
             let newDb = db.filter(function(note){
                 return note.id != specificNoteId;
